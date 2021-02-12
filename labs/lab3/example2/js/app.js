af27884e38b80da8e45dce30222f086b2fc8fc47
BryceKoehl <63129946+BryceKoehl@users.noreply.github.com>
2021-02-11 21:05:31 -0500

    fixed example 2

--- labs/lab3/example2/js/app.js
+++ labs/lab3/example2/js/app.js
@@ -1,3 +1,32 @@
+// 2 - Pyramid
+
+let posX = 20;
+let posY = 20;
+
+function setup() {
+    createCanvas(400, 250);
+  }
+
+  function draw() {
+    // background(0);
+       
+
+    for (r = 1; r <= 4; ++r) {
+      for (s = 1; s <= r; ++s) {
+        fill(250, 0, 0, 250)
+        noStroke()
+		    square(posX,posY,50)
+        posX = posX + 60
+      }
+      posY = posY + 60
+      posX = 20;		
+	}
+
+}
+
+
+
+
 
 
 
