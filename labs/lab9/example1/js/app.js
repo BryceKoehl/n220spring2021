b42ddc2f325a9bf6de0a9b3f0511a301145cbf93
BryceKoehl <63129946+BryceKoehl@users.noreply.github.com>
2021-04-08 23:56:13 -0400

    Add files via upload

--- labs/lab9/example1/js/app.js
+++ labs/lab9/example1/js/app.js
@@ -1,16 +1,20 @@
 //1 - Peak Pixels
 
 let dvRect = document.getElementById("rect");
+
 let h = 100;
 let w = 100;
-
-function increase(){
-    h = h*1.1;
-    w = w*1.1;
-}
 
 dvRect.style.width = w + "px";
 dvRect.style.height = h + "px";
 dvRect.style.backgroundColor = "green";
+
+function increase(){
+    h = h*1.1;
+    w = w*1.1;
+
+    dvRect.style.width = w + "px";
+    dvRect.style.height = h + "px";
+}
 
 
