c252373c909236b7d9f99e78bbbeb28307291d98
BryceKoehl <63129946+BryceKoehl@users.noreply.github.com>
2021-05-03 10:30:53 -0400

    final

--- final/js/app.js
+++ final/js/app.js
@@ -1,50 +1,159 @@
-//2 - Color Mixer
-let dvRect = document.getElementById("rect");
-let dvCurrentColor = document.getElementById("currentColor");
+//REFERENCES
+showWord = document.getElementById("showWord");
+shouPrompt = document.getElementById("showPrompt");
+space = document.getElementById("space");
 
-let red = 0;
-let green = 0;
-let blue = 0;
+//array for word choices
+let words = ["cat", "inflated", "crash", "royal", "sun", "space", "science", "silicon", "egypt", "jeuel","syzygy"];
+randomChoice = words [Math.floor(Math.random()*words. length)]; //randomly pulls a word from the array
+console. log(randomChoice);
 
+//variables
+let totalCorrect = 0;
+let totalIncorrect = 0;
+let blanks = [];
 
+let splitWord = randomChoice.split(''); //splits chosen word into characters
+//DisplayWord.InnerHTML = randonChoice;
+showWord.innerHTHL = "Hangman";
+console.log(splitWord);
 
-function addColor(event) {
-    //get the color
-    let getColor = event.target.getAttribute("data-color");
-    //get the amount of color
-    let getWeight = event.target.getAttribute("data-weight");
-
-    if(getColor == "red") {
-        if(getWeight == 1){
-            red += 1;   
-        } else if(getWeight == 5){
-            red += 5;   
-        } else if(getWeight == 10){
-            red += 10;   
-        }
-    } 
-
-    if(getColor == "green") {
-        if(getWeight == 1){
-            green += 1;   
-        } else if(getWeight == 5){
-            green += 5;   
-        } else if(getWeight == 10){
-            green += 10;   
-        }
-    } 
-
-    if(getColor == "blue") {
-        if(getWeight == 1){
-            blue += 1;   
-        } else if(getWeight == 5){
-            blue += 5;   
-        } else if(getWeight == 10){
-            blue += 10;   
-        }
-    } 
-    endColor = "rgb(" + red + "," + green + "," + blue + ")";
-    dvCurrentColor.innerHTML ="currentColor " + endColor;
-    dvRect.style.backgroundColor = endColor;
+// reset the page
+function reset() {
+    location. reload();
 }
 
+for(i = 0; i < splitWord.length; i++) {
+var blank = document.createElement("div"); //creates buttons
+blank.classList.add("space");
+blank.style.left = 700 + i*50 + "px";
+space.appendChild(blank);
+blanks.push(blank);
+}
+console.log(blanks);
+
+//array for letters
+let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+//generates letters on the page
+for(i = 0; 1 < letters.length; i++) {
+    let button = document.createElement("button"); //creates buttons
+    button.innerHTL = letters[i]; //assigns a letter to each button of the array
+    button.classList.add("buttons");
+    let buttonDiv = document.getElementById("buttons");
+    buttonDiv.appendChild(button);
+
+    button.addEventListener("click", asClicked); //listens for an event
+    button.addEventListener("click", filterText); //listens for event with a different function to Invoke
+
+    function asClicked(event) { //when the mouse is clicked
+        let choice = event.target.innerHTML; //choice is equal to the event.target.innerHTML (aka: a,b,c,etc)
+        event.target.disebled = true; //disables the selection of a letter after it has been selected once
+        //console.log(event.target.innerHTHL);
+    }
+
+    function filterText(event){ //when the mouse is clicked
+        //IF IT IS A MATCH
+        let splitChoice = randomChoice.split('');
+        //consote.log("TESTING: " + splitChoice);
+    }
+
+    let found = false;
+    for(i = 0; i < randomChoice.length; i++) {
+        if(event.target.innerHTML == splitChoice[i]) {
+        showPrompt.InnerHTHL = (event.target.innerHTML + " is in the word");
+        totalCorrect += 1;
+        found = true;
+        //console.log(totalCorrect);
+
+        //it you win
+        blanks[i].innerHTML = event.target.innerHTML; 
+        if(totalCorrect >= randomChoice.length){
+            showPrompt.InnerHTHL = ("You win!");
+            break;
+            }
+        }
+    }
+    //if it's not a match
+    if(!found) {
+        showPrompt.innerHTML = (event.target.innerHTML + " is not in the word");
+        totalIncorrect += 1;
+        //console.log(totalIncorrect);
+        if(totalIncorrect == 6) {
+            showPrompt.innerHTML = ("You lose, click reset to try again!");
+        } else if(totalIncorrect >= 6) {
+        location.reload();
+        }
+    }
+}
+
+//PS
+function setup() {
+    createCanvas (1375, 500);
+    background (35, 61, 88);
+    //rectModetCENTER);
+}
+
+function drau(event){
+background(35, 61, 88);
+noStroke();
+fill(255,255,255);
+
+if (totalCorrect == 1){
+    noStroke();
+    fill(191, 215, 255);
+    circle(300, 120, 90); //head
+}else if(totalCorrect == 2){
+    noStroke();
+    fill(191, 215, 255);
+    circle(300, 120, 90); //head
+    fill(155, 177, 255);
+    rect(290, 163, 20, 139); //spine
+}else if(totalCorrect == 3){
+    noStroke();
+    fill(191, 215, 255);
+    circle(300, 120, 90); //head
+    fill(155, 177, 255);
+    rect(290, 163, 20, 139); //spine
+    fill(155, 177, 255);
+    rect (230,280,60,20); //left arm
+}else if(totalCorrect == 4){
+    noStroke();
+    fill(191, 215, 255);
+    circle(300, 120, 90); //head
+    fill(155, 177, 255);
+    rect(290, 163, 20, 139); //spine
+    fill(155, 177, 255);
+    rect (230,280,60,20); //left arm
+    fill(155,177,255);
+    rect(310, 199,60,20); //right am
+}else if (totalCorrect == 5){
+    noStroke();
+    fill(191, 215, 255);
+    circle(300, 120, 90); //head
+    fill(155, 177, 255);
+    rect(290, 163, 20, 139); //spine
+    fill(155, 177, 255);
+    rect (230,280,60,20); //left arm
+    fill(155,177,255);
+    rect(310, 199,60,20); //right am
+    rotate(PI / 3.0);
+    fill(155, 177, 255);
+    rect (400, -117, 20, 90); //left leg
+} else if (totalIncorrect == 6){
+    noStroke();
+    fill(191, 215, 255);
+    circle(300, 120, 90); //head
+    fill(155, 177, 255);
+    rect(290, 163, 20, 139); //spine
+    fill(155, 177, 255);
+    rect (230,280,60,20); //left arm
+    fill(155,177,255);
+    rect(310, 199,60,20); //right am
+    rotate(PI / 3.0);
+    fill(155, 177, 255);
+    rect (400, -117, 20, 90); //left leg
+    fill(155, 177, 255);
+    rect(405,-123,90,20); //right leg
+}
+}
